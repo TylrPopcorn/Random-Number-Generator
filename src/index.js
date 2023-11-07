faea47d474ed915b569ffa9a91f15573e1b1d7df
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-11-07 14:55:06 -0500

    Setting up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * npm install parcel-bundler
+ * npm install react
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+//functions:
+import App from "./App";
