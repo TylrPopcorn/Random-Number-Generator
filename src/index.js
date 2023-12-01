4cabe70c37c5ca2516cbcfb463b60985749c4663
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-11-30 21:58:44 -0500

    Setting up

--- src/index.js
+++ src/index.js
@@ -9,3 +9,10 @@ import "./styles.css";
 
 //functions:
 import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
