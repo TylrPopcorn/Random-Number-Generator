acc1de68f98613a13e8f280b0e6c8da2c126d276
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-12-03 21:22:21 -0500

    Working on styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [num, setNum] = useState(0);
+
+  const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(randomNumber);
+  };
+
+  return (
+    <div className="ButtonHolder">
+      <p>{num}</p>
+      <button className="GenerateButton" onClick={generateNumber}>
+        Generate
+      </button>
+      <p>Click the button above to generate a random number</p>
+    </div>
+  );
 }
+
+export default App;
